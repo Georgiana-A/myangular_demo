2e709a065989d6833b68bfa044ebbd115505772e
Georgiana <g.apristavitei@gmail.com>
2017-08-11 15:13:31 +0300

    all app commited

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'msg-app',
+  templateUrl : 'app/app.component.html',
+  styleUrls: ['app/app.component.css']
+})
+
+export class AppComponent {
+  message = "You did it!!!! Huuuraaaayyyy!!!!";
+}
